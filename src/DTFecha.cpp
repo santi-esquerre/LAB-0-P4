9ef511e82cc5be70f43acea1611898a8e86ebc07
Danny <danielmotta369@gmail.com>
2025-03-18 19:05:15 -0300

    Se agrego operador >= a Fecha

---
+++ src/DTFecha.cpp
@@ -0,0 +1,9 @@
+#include DTFecha.h 
+
+bool DTFecha::operator>=(DTFecha fecha) {
+    int anio = fecha.getAnio();
+    int mes = fecha.getMes();
+    int dia = fecha.getDia();
+
+    return (this->anio > anio) || (this->anio == anio && this->mes > mes) || (this->anio == anio && this->mes == mes && this->dia >= dia);
+}
