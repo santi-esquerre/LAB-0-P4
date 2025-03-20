fd6b45f7281230e43ceb17ed32171fb0ef224be0
Juanifalero <juanifalero@gmail.com>
2025-03-20 12:41:08 -0300

    Add files via upload

--- src/DTRefer.cpp
+++ src/DTRefer.cpp
@@ -1,46 +1,38 @@
 #include "../header/DTRefer.h"
-// #include <iostream>
-// #include <string>
-// #include <set>
-// using namespace std;
 
-DTRefer::DTRefer(const string &DOI, const string &titulo, DTFecha &fecha, set<string> &autores){
+
+DTRefer::DTRefer(const std::string &DOI, const std::string &titulo, DTFecha &fecha, std::set<std::string> &autores){
     this->DOI = DOI;
     this->titulo = titulo;
     this->fecha = fecha;
     this->autores = autores;
-} // Aca capaz que seria bueno cambiar alguna mayuscula en los argumentos de la funcion para que se lean distintos a los elementos de la clase
+}
 
 DTRefer::DTRefer(){
     this->DOI = "";
     this->titulo = "";
     this->fecha = DTFecha();
-    this->autores = set<string> {""};
+    this->autores = std::set<std::string> {""};
 }
-
-// DTRefer::~DTRefer(){
-
-// }
-
 
 //Getters
 
-string DTRefer::getDOI(){
+std::string DTRefer::getDOI(){
     return this->DOI;
 }
-string DTRefer::getTitulo(){
+std::string DTRefer::getTitulo(){
     return this->titulo;
 }
 DTFecha DTRefer::getFecha(){
     return this->fecha;
 }
-set<string> DTRefer::getAutores(){
+std::set<std::string> DTRefer::getAutores(){
     return this->autores;
 }
 
-ostream &operator<<(ostream &os, DTRefer &dtRefer){
+std::ostream &operator<<(std::ostream &os, DTRefer &dtRefer){
     os << dtRefer.getDOI() << "->" << dtRefer.getTitulo() << "(" << dtRefer.getFecha().getDia() << "/" << dtRefer.getFecha().getMes() << dtRefer.getFecha().getAnio() << ")/";
-    for (string autor : dtRefer.getAutores()){
+    for (std::string autor : dtRefer.getAutores()){
         os << autor << ",";
     }
     return os;
