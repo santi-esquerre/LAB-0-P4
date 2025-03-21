fcad70174a67028c1ecd174c0fb764a6dba63240
alexjhoel <alexanderalmeida20@gmail.com>
2025-03-21 07:48:58 -0300

    [WIP] main.cpp pasos a - h

--- main.cpp
+++ main.cpp
@@ -18,8 +18,8 @@ std::map<std::string, Investigador*> map_investigadores;
 
 void coleccion_guardarPublicacion(Publicacion* pub){
 	publicaciones.push_back(pub);
-	std::pair<std::string, Publicacion*> entry(pub->getDOI(), pub);
-    map_publicaciones.insert(entry);
+	std::pair<std::string, Publicacion*> nuevaEntrada(pub->getDOI(), pub);
+    map_publicaciones.insert(nuevaEntrada);
 }
 void coleccion_eliminarPublicacion(Publicacion* pub){
 	publicaciones.remove(pub);
@@ -28,8 +28,8 @@ void coleccion_eliminarPublicacion(Publicacion* pub){
 
 void coleccion_guardarInvestigador(Investigador* inv){
 	investigadores.push_back(inv);
-	std::pair<std::string, Investigador*> entry(inv->getORCID(), inv);
-    map_investigadores.insert(entry);
+	std::pair<std::string, Investigador*> nuevaEntrada(inv->getORCID(), inv);
+    map_investigadores.insert(nuevaEntrada);
 }
 
 Investigador* coleccion_getInvestigador(std::string ORCID){
@@ -41,58 +41,115 @@ Publicacion* coleccion_getPublicacion(std::string DOI){
 }
 
 void main() {
+        //Parte A
+        cout << "Parte A" << endl;
+
         //Artículo 1
-        DTFecha* fecha1 = new DTFecha(15, 5, 2023);
+        DTFecha fecha1(15, 5, 2023);
 
         ArticuloRevista * art1 = new ArticuloRevista("10.1234/abc123", "Fundamentos de POO", fecha1, "Programación Avanzada","Introduccion a los principios fundamentales de la programacion orientada a objetos, explicando sus conceptos clave como clases, objetos, herencia y polimorfismo.");
         
         // Artículo 2
-        DTFecha* fecha2 = new DTFecha(10, 2, 2024);
+        DTFecha fecha2(10, 2, 2024);
 
-        ArticuloRevista* art2 = new ArticuloRevista("10.4567/jkl012", "Utilidad de diagramas UML", *fecha2, "Modelado de Software", "Ejercicio empirico de como los diagramas UML pueden ayudar en el proceso y documentacion de software, cubriendo los tipos mas importantes utilizados, como clases.");
+        ArticuloRevista* art2 = new ArticuloRevista("10.4567/jkl012", "Utilidad de diagramas UML", fecha2, "Modelado de Software", "Ejercicio empirico de como los diagramas UML pueden ayudar en el proceso y documentacion de software, cubriendo los tipos mas importantes utilizados, como clases.");
+        
+
+        //Parte B
+        cout << "Parte B" << endl;
 
         // Libro 1
-        DTFecha* fecha3 = new DTFecha(20, 8, 2022);
-        set<string>* keyWords1 = new set<string>({"Diseno", "OOP", "Class"});
+        DTFecha fecha3(20, 8, 2022);
+        set<string> keyWords1 = {"Diseno", "OOP", "Class"};
 
-        Libro* libro1 = new Libro("10.2345/def456", "Patrones de Diseno en c++", *fecha3, "Software Design", *keyWords1);
+        Libro* libro1 = new Libro("10.2345/def456", "Patrones de Diseno en c++", fecha3, "Software Design", keyWords1);
 
         // Libro 2
-        DTFecha* fecha4 = new DTFecha(20, 8, 2022);
-        set<string>* keyWords2 = new set<string>({"Diagramas", "UML", "Software", "Modelado"});
+        DTFecha fecha4(20, 8, 2022);
+        set<string> keyWords2 = {"Diagramas", "UML", "Software", "Modelado"};
 
-        Libro* libro2 = new Libro("10.5678/mno345", "Guia de UML", *fecha4, "IEEE", *keyWords2);
+        Libro* libro2 = new Libro("10.5678/mno345", "Guia de UML", fecha4, "IEEE", keyWords2);
+
+
+        //Parte C
+        cout << "Parte C" << endl;
 
         // Página Web 1
-        DTFecha* fecha5 = new DTFecha(20, 10, 2024);
+        DTFecha fecha5(20, 10, 2024);
 
-        PaginaWeb* paginaWeb = new PaginaWeb("10.3456/ghi789", "Diagramas para Principiantes", *fecha5, "www.umlparaprincipiantes.com", "En esta pagina web se presenta una gui completa sobre los diagramas UML, abordando los diagramas de casos de uso, de clases, de secuencia y de actividades.");
+        PaginaWeb* paginaWeb = new PaginaWeb("10.3456/ghi789", "Diagramas para Principiantes", fecha5, "www.umlparaprincipiantes.com", "En esta pagina web se presenta una gui completa sobre los diagramas UML, abordando los diagramas de casos de uso, de clases, de secuencia y de actividades.");
 
         //Agregar publicaciones al conjunto
         //TO DO: Hacerlo con set
         coleccion_guardarPublicacion(art1);
         coleccion_guardarPublicacion(art2);
         
+
+        //Parte D
+        cout << "Parte D" << endl;
+
         // Imprimir publicaciones
-        for (int i = 0; i < publicaciones.size(); i++)
+        for (list<Publicacion*>::iterator publi = publicaciones.begin(); publi != publicaciones.end(); ++publi)
         {
-            cout << publicaciones[i].getDT() << endl;
+            cout << (*publi)->getDT() << endl;
         }
-        
+
+
+        //Parte E
+        cout << "Parte E" << endl;
 
         // Crear investigadores
-        Investigador investigador1("0000-0003-1234-5678", "Carla Oliveri", "Universidad de la Republica");
-        Investigador investigador2("0000-0001-8765-4321", "Alberto Santos", "Instituto Tecnico");
+        Investigador * investigador1 = new Investigador("0000-0003-1234-5678", "Carla Oliveri", "Universidad de la Republica");
+        Investigador * investigador2 = new Investigador("0000-0001-8765-4321", "Alberto Santos", "Instituto Tecnico");
 
         // Agregar investigadores al conjunto
-        investigadores.push_back(investigador1);
-        investigadores.push_back(investigador2);
+        coleccion_guardarInvestigador(investigador1);
+        coleccion_guardarInvestigador(investigador2);
+
+
+        //Parte F
+        cout << "Parte F" << endl;
 
         // Imprimir investigadores
-        for (int i = 0; i < investigadores.size(); i++)
+        for (list<Investigador*>::iterator inv = investigadores.begin(); inv != investigadores.end(); ++inv)
         {
-            cout << investigadores[i].toString() << endl;
+            cout << (*inv)->toString() << endl;
         }
+
+
+        //Parte G
+        cout << "Parte G" << endl;
+
+        // Agregar publicaciones a investigadores
+        coleccion_getInvestigador("0000-0003-1234-5678")->agregarPublicacion(coleccion_getPublicacion("10.1234/abc123"));
+        coleccion_getInvestigador("0000-0003-1234-5678")->agregarPublicacion(coleccion_getPublicacion("10.4567/jkl012"));
+        coleccion_getInvestigador("0000-0003-1234-5678")->agregarPublicacion(coleccion_getPublicacion("10.5678/mno345"));
+        coleccion_getInvestigador("0000-0003-1234-5678")->agregarPublicacion(coleccion_getPublicacion("10.3456/ghi789"));
+        coleccion_getInvestigador("0000-0001-8765-4321")->agregarPublicacion(coleccion_getPublicacion("10.1234/abc123"));
+        coleccion_getInvestigador("0000-0001-8765-4321")->agregarPublicacion(coleccion_getPublicacion("10.2345/def456"));
+        coleccion_getInvestigador("0000-0001-8765-4321")->agregarPublicacion(coleccion_getPublicacion("10.4567/jkl012"));
+
+        // Agregar investigadores a publicaciones
+        coleccion_getPublicacion("10.1234/abc123")->agregarAutor(coleccion_getInvestigador("0000-0003-1234-5678"));
+        coleccion_getPublicacion("10.1234/abc123")->agregarAutor(coleccion_getInvestigador("0000-0001-8765-4321"));
+        coleccion_getPublicacion("10.4567/jkl012")->agregarAutor(coleccion_getInvestigador("0000-0003-1234-5678"));
+        coleccion_getPublicacion("10.4567/jkl012")->agregarAutor(coleccion_getInvestigador("0000-0001-8765-4321"));
+        coleccion_getPublicacion("10.5678/mno345")->agregarAutor(coleccion_getInvestigador("0000-0003-1234-5678"));
+        coleccion_getPublicacion("10.2345/def456")->agregarAutor(coleccion_getInvestigador("0000-0001-8765-4321"));
+        coleccion_getPublicacion("10.3456/ghi789")->agregarAutor(coleccion_getInvestigador("0000-0003-1234-5678"));
+
+
+        //Parte H
+        cout << "Parte H" << endl;
+
+        
+        Publicacion * pubAEliminar = coleccion_getPublicacion("10.4567/jkl012");
+        coleccion_eliminarPublicacion(pubAEliminar);
+        delete(pubAEliminar);
+
+        
+
+
 
 
 
